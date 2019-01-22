ca2625ff0791c9ed9dee611821803625fe106c9b
Ferenc Béres <ferdzso05@gmail.com>
2019-01-22 10:25:31 +0100

    dummy flask app

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+#Create instance of Flask App
+app = Flask(__name__)
+
+#Define Route
+@app.route("/")
+
+#Content
+def home():
+    return("Home Page")
+
+#Running and Controlling the script
+if (__name__ =="__main__"):
+	app.run(debug=True)
